219558572cd3105305af3aa7676d47f526cd1e54
Kevin Reedy <kevinreedy@gmail.com>
2016-12-06 14:50:28 -0600

    test of rhel version pin

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,4 +22,7 @@ case node['platform_family']
 when 'windows'
   default['push_jobs']['package_url']      = 'https://opscode-private-chef.s3.amazonaws.com/windows/2008r2/x86_64/opscode-push-jobs-client-windows-1.1.5-1.windows.msi'
   default['push_jobs']['package_checksum'] = '411520e6a2e3038cd018ffacee0e76e37e7badd1aa84de03f5469c19e8d6c576'
+when 'rhel'
+  default['push_jobs']['package_url']      = 'https://packages.chef.io/stable/el/7/push-jobs-client-1.3.4-1.el7.x86_64.rpm'
+  default['push_jobs']['package_checksum'] = '5c449530fcea87f6d8855a659fa7bb62376fad9a51171198fe4a2e0c9d3ba001'
 end
